3ed0912432828dd4babd04883504f9d5b2b86ea9
Paul Lawlor <paul.lawlor@and.digital>
2022-09-14 11:18:57 +0100

    Initialise repo

---
+++ src/main/java/com/example/paymentsBackend/PaymentsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.paymentsBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentsBackendApplication.class, args);
+	}
+
+}
